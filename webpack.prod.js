0506dda198b39dafb522fe6796a43ccd57f6d2ba
taki.akinori <taki.akinori@synergy101.jp>
2018-07-27 17:47:44 +0900

    react関連を外す

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,4 +7,46 @@ config.performance = {
   hints: false,
 };
 
+// 製品ビルドにはReact関連のコードを除外
+config.externals = {
+  'react': {
+    root: 'React',
+    commonjs2: 'react',
+    commonjs: 'react',
+    amd: 'react'
+  },
+  './react': {
+    root: 'React',
+    commonjs2: 'react',
+    commonjs: 'react',
+    amd: 'react'
+  },
+  'react-dom': {
+    root: 'ReactDOM',
+    commonjs2: 'react-dom',
+    commonjs: 'react-dom',
+    amd: 'react-dom'
+  },
+  './react-dom': {
+    root: 'ReactDOM',
+    commonjs2: 'react-dom',
+    commonjs: 'react-dom',
+    amd: 'react-dom'
+  },
+  'react-transition-group': {
+    root: 'ReactTransitionGroup',
+    commonjs2: 'react-transition-group',
+    commonjs: 'react-transition-group',
+    amd: 'react-transition-group'
+  },
+  './react-transition-group': {
+    root: 'ReactTransitionGroup',
+    commonjs2: 'react-transition-group',
+    commonjs: 'react-transition-group',
+    amd: 'react-transition-group'
+  },
+};
+// 製品ビルドはUMDモジュールとして出力
+config.output.libraryTarget = "umd";
+
 module.exports = config;
